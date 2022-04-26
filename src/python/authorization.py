8c5e82ee19af595577cd3f7a623e59ff24cac543
KubaBoi <jakubanderle@outlook.cz>
2022-04-26 19:36:08 +0200

    cheese update

--- src/python/authorization.py
+++ src/python/authorization.py
@@ -6,15 +6,6 @@ from cheese.appSettings import Settings
 #@authorization enabled
 class Authorization:
 
-    authExceptions = [
-        ["GET", "/main/init"],
-        ["GET", "/main/favorites"],
-        ["GET", "*.html"],
-        ["GET", "*.css"],
-        ["GET", "*.js"],
-        ["GET", "*.png"]
-    ]
-
     @staticmethod
     def authorize(server, path, method):
         if (Authorization.isException(method, path)):
@@ -44,12 +35,13 @@ class Authorization:
     def isException(method, path):
         pathNoArgs = cc.getPath(path)
 
-        for exception in Authorization.authExceptions:
-            if (exception[0] == method):
-                excpPath = exception[1].replace("*", "")
-                if (exception[1].startswith("*")):
+        for exception in Settings.authExcepts:
+            if (exception["method"] == method):
+
+                excpPath = exception["path"].replace("*", "")
+                if (exception["path"].startswith("*")):
                     if (pathNoArgs.endswith(excpPath)): return True
-                elif (exception[1].endswith("*")):
+                elif (exception["path"].endswith("*")):
                     if (pathNoArgs.startswith(excpPath)): return True
                 else:
                     if (pathNoArgs == excpPath): return True
